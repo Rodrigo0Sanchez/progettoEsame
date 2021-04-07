d0a7425a7fdcde3a275cbcfeee9b633d8ee58840
Rodrigo0Sanchez <sanchez.salinas.gianpier.rodrigo@iisgalvanimi.edu.it>
2021-04-07 10:26:07 +0000

    Traduzione

--- project/src/app/api.service.ts
+++ project/src/app/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
 
   salt: string;
   loggedin: boolean = false;
-  baseUrl = `https://3000-bronze-jaguar-c686i5wg.ws-eu03.gitpod.io/`;
+  baseUrl = `https://3000-pink-crawdad-51ixie2y.ws-eu03.gitpod.io/`;
 
   constructor(private http: HttpClient) { }
 
